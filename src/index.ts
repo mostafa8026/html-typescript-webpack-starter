91ccfe1bb4638063815456e1aaaf1b7d4d75d53e
Mostafa <mostafa8026@gmail.com>
2023-05-19 21:22:04 +0330

    feat: make it more beatiful and add assets

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import "./style.scss";
 
 import * as _ from 'lodash';
+import blackholeImg from './assets/blackhole.jpg';
 
 console.log("Hello World From Console!");
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
+  element.className = 'abstract_font'
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+  return element;
 }
 
-document.body.appendChild(component());
+function blackholeComponent() {
+  const element = document.createElement('img');
+  element.className = 'abstract_img';
+  element.src = blackholeImg;
+  return element;
+}
+
+/**
+ * Main div at the center of the page
+ */
+const mainDiv = document.createElement('div');
+mainDiv.className = 'main';
+document.body.appendChild(mainDiv);
+
+
+mainDiv.appendChild(component());
+mainDiv.appendChild(blackholeComponent());
